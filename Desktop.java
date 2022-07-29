6d81dca601679b0a27bbd00fa010589d91677359
Babyganesh <109017980+Babyganesh@users.noreply.github.com>
2022-07-29 20:01:08 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,19 @@
+package org.system;
+
+public class Desktop extends Computer {
+	public void desktopSize() {
+		// TODO Auto-generated method stub
+	}
+		public void computerModel() {
+
+		}
+
+
+
+	}
+
+
+
+	
+
+
